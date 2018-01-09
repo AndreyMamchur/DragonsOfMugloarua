4c3b1bfa0d810b71bdb09b79df913c272ad1e404
AndreyMamchur <mamchurandrey87@gmail.com>
2018-01-09 12:09:06 +0200

    Add class Knight.

---
+++ src/main/java/ua/DragonsOFMugolar/entity/Knight.java
@@ -0,0 +1,60 @@
+package ua.DragonsOFMugolar.entity;
+
+public class Knight {
+    private String name;
+    private int attack;
+    private int armor;
+    private int agility;
+    private int endurance;
+
+    public Knight() {
+    }
+
+    public Knight(String name, int attack, int armor, int agility, int endurance) {
+        this.name = name;
+        this.attack = attack;
+        this.armor = armor;
+        this.agility = agility;
+        this.endurance = endurance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+}
