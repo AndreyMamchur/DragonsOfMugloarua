168673bea58d3ec3fc1310dc55c7f7f4eb817dc4
AndreyMamchur <mamchurandrey87@gmail.com>
2018-01-09 12:45:42 +0200

    Add class Dragon.

---
+++ src/main/java/ua/DragonsOFMugolar/entity/Dragon.java
@@ -0,0 +1,50 @@
+package ua.DragonsOFMugolar.entity;
+
+public class Dragon {
+    private int scaleThickness;
+    private int clawSharpness;
+    private int wingStrength;
+    private int fireBreath;
+
+    public Dragon() {
+    }
+
+    public Dragon(int scaleThickness, int clawSharpness, int wingStrength, int fireBreath) {
+        this.scaleThickness = scaleThickness;
+        this.clawSharpness = clawSharpness;
+        this.wingStrength = wingStrength;
+        this.fireBreath = fireBreath;
+    }
+
+    public int getScaleThickness() {
+        return scaleThickness;
+    }
+
+    public void setScaleThickness(int scaleThickness) {
+        this.scaleThickness = scaleThickness;
+    }
+
+    public int getClawSharpness() {
+        return clawSharpness;
+    }
+
+    public void setClawSharpness(int clawSharpness) {
+        this.clawSharpness = clawSharpness;
+    }
+
+    public int getWingStrength() {
+        return wingStrength;
+    }
+
+    public void setWingStrength(int wingStrength) {
+        this.wingStrength = wingStrength;
+    }
+
+    public int getFireBreath() {
+        return fireBreath;
+    }
+
+    public void setFireBreath(int fireBreath) {
+        this.fireBreath = fireBreath;
+    }
+}
