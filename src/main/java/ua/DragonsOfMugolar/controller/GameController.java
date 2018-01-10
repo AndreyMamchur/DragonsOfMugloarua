4a60bafaf4d6057130a0392a6432d3d6ca572914
AndreyMamchur <mamchurandrey87@gmail.com>
2018-01-10 15:53:08 +0200

    Create class GameController.

---
+++ src/main/java/ua/DragonsOfMugolar/controller/GameController.java
@@ -0,0 +1,29 @@
+package ua.DragonsOfMugolar.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.DragonsOfMugolar.service.GameService;
+
+@Controller
+@RequestMapping("/")
+public class GameController {
+
+    private static final Logger logger = Logger.getLogger(GameController.class);
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/startGame")
+    public String startGame(){
+        gameService.startGame();
+        return "game";
+    }
+}
