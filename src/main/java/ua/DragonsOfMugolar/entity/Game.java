93d6fb1244122824d20e3e765761c798d8188cf2
AndreyMamchur <mamchurandrey87@gmail.com>
2018-01-12 10:34:16 +0200

    Simplified object Game.

--- src/main/java/ua/DragonsOfMugolar/entity/Game.java
+++ src/main/java/ua/DragonsOfMugolar/entity/Game.java
@@ -1,38 +1,11 @@
 package ua.DragonsOfMugolar.entity;
 
-import java.util.List;
-
 public class Game {
     private long gameId;
-    private int allBattle;
-    private int victoryBattle;
-    private int defeatBattle;
     private Knight knight;
     private Dragon dragon;
 
     public Game() {
-    }
-
-    public Game(int allBattle, int victoryBattle, int defeatBattle) {
-        this.allBattle = allBattle;
-        this.victoryBattle = victoryBattle;
-        this.defeatBattle = defeatBattle;
-    }
-
-    public Game(long gameId, int allBattle, int victoryBattle, int defeatBattle) {
-        this.gameId = gameId;
-        this.allBattle = allBattle;
-        this.victoryBattle = victoryBattle;
-        this.defeatBattle = defeatBattle;
-    }
-
-    public Game(long gameId, int allBattle, int victoryBattle, int defeatBattle, Knight knight, Dragon dragon) {
-        this.gameId = gameId;
-        this.allBattle = allBattle;
-        this.victoryBattle = victoryBattle;
-        this.defeatBattle = defeatBattle;
-        this.knight = knight;
-        this.dragon = dragon;
     }
 
     public long getGameId() {
@@ -41,30 +14,6 @@ public class Game {
 
     public void setGameId(long gameId) {
         this.gameId = gameId;
-    }
-
-    public int getAllBattle() {
-        return allBattle;
-    }
-
-    public void setAllBattle(int allBattle) {
-        this.allBattle = allBattle;
-    }
-
-    public int getVictoryBattle() {
-        return victoryBattle;
-    }
-
-    public void setVictoryBattle(int victoryBattle) {
-        this.victoryBattle = victoryBattle;
-    }
-
-    public int getDefeatBattle() {
-        return defeatBattle;
-    }
-
-    public void setDefeatBattle(int defeatBattle) {
-        this.defeatBattle = defeatBattle;
     }
 
     public Knight getKnight() {
